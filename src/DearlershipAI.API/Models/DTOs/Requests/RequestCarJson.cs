389ba710774a7d3963c1f169f554d9584b821727
SamuelTorres51 <samuel.torres28@hotmail.com>
2026-04-02 09:28:16 -0300

    feat: modelando as classes necessarias

---
+++ src/DearlershipAI.API/Models/DTOs/Requests/RequestCarJson.cs
@@ -0,0 +1,13 @@
+﻿namespace DearlershipAI.API.Models.DTOs.Requests;
+
+public class RequestCarJson {
+    public string Brand { get; set; } = string.Empty;
+    public string Model { get; set; } = string.Empty;
+    public string Version { get; set; } = string.Empty;
+    public int Year { get; set; }
+    public decimal Price { get; set; }
+    public string Fuel { get; set; } = string.Empty;
+    public string Transmissao { get; set; } = string.Empty;
+    public int Mileage { get; set; }
+    public string? Image_url { get; set; } = string.Empty;
+}
