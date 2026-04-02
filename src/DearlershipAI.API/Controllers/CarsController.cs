64c4161bc0cef11526639e27366c924bcdad1c92
SamuelTorres51 <samuel.torres28@hotmail.com>
2026-04-02 10:56:03 -0300

    feat: finalizando controller da rota

--- src/DearlershipAI.API/Controllers/CarsController.cs
+++ src/DearlershipAI.API/Controllers/CarsController.cs
@@ -1,5 +1,6 @@
 ﻿using DearlershipAI.API.Models.DTOs.Requests;
-using Microsoft.AspNetCore.Http;
+using DearlershipAI.API.Models.Entities;
+using DearlershipAI.API.Services.UseCases.Cars.Create;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DearlershipAI.API.Controllers;
@@ -8,8 +9,12 @@ namespace DearlershipAI.API.Controllers;
 [ApiController]
 public class CarsController : ControllerBase {
     [HttpPost]
-    public async Task<IActionResult> AddCar([FromBody] RequestCarJson request,) {
-    
-    
+    [ProducesResponseType(typeof(Car), StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> AddCar([FromBody] RequestCarJson request, [FromServices] ICreateCarUseCase useCase){
+        var response = await useCase.Execute(request);
+        if(response == null) 
+            return BadRequest("Failed to create car.");
+        return Created(string.Empty, response);
     }
 }
